ee56d36d560da23d2519d2d67cbfae3c3377bf36
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-27 23:38:35 -0400

    remove react-pixi

--- src/components/Gameboard/Gameboard.tsx
+++ src/components/Gameboard/Gameboard.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 /* Subcomponents */
 import { Tile } from "../";
+/* Constants */
+import { MAX_ROWS, MAX_COLUMS } from "../../constants/gameConfig";
 
 const Gameboard = () => {
+  // const renderTiles = () => {
+  //   for (let )
+  // }
+
   return (
     <div>
       <h3>from Gameboard.tsx</h3>
