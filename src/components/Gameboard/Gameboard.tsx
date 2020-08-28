e1f5789744b9fee91321f3fe7731e6e7cc45b140
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-28 00:04:20 -0400

    testing deploy

--- src/components/Gameboard/Gameboard.tsx
+++ src/components/Gameboard/Gameboard.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 /* Subcomponents */
 import { Tile } from "../";
+/* Pixi */
+import * as Pixi from "pixi.js";
 /* Constants */
-import { MAX_ROWS, MAX_COLUMS } from "../../constants/gameConfig";
+// import { MAX_ROWS, MAX_COLUMS } from "../../constants/gameConfig";
+import { tilePath } from "../../constants/imagePaths";
 
 const Gameboard = () => {
-  // const renderTiles = () => {
-  //   for (let )
-  // }
+  const boardContainerRef = useRef<HTMLDivElement>(null);
+  let app: Pixi.Application;
+
+  // when component mounts, initialize board
+  useEffect(() => {
+    app = new Pixi.Application();
+    if (null !== boardContainerRef.current) {
+      boardContainerRef.current.appendChild(app.view);
+    }
+  }, []);
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    app.stage.addChild(Pixi.Sprite.from(tilePath));
+  };
+
+  console.log("Mounted");
 
   return (
     <div>
       <h3>from Gameboard.tsx</h3>
       <Tile />
+
+      <button onClick={handleClick}>Add tower</button>
+
+      <div ref={boardContainerRef}></div>
     </div>
   );
 };
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -6,7 +6,7 @@ const Tile = () => {
   return (
     <div>
       <h3>from Tile.tsx</h3>
-      <img src={tilePath} />
+      <img src={tilePath} alt="tile" />
     </div>
   );
 };
