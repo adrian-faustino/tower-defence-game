56705338c4869fd995f7705b75b1970b00ff40a2
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-27 23:45:38 -0400

    modularized image paths

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+/* Constants */
+import { tilePath } from "../../constants/imagePaths";
 
 const Tile = () => {
   return (
     <div>
       <h3>from Tile.tsx</h3>
+      <img src={tilePath} />
     </div>
   );
 };
---
+++ src/constants/imagePaths.tsx
@@ -0,0 +1,1 @@
+export const tilePath = "tile-assets/_ground/ground05.png";
