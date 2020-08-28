513eb3da2f2c22400b1db785281e4a555915028a
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-27 22:40:44 -0400

    folder setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+/* Styles */
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>from App.tsx</h3>
+    </div>
+  );
+}
+
+export default App;
