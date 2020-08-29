c75e4ea28c4f9aa2f4165247bef7c4b9e730d1dd
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-28 20:16:42 -0400

    added grid

---
+++ src/game/index.ts
@@ -0,0 +1,32 @@
+/* Util */
+import { drawUtils } from "../util/canvasDrawHelpers";
+import { gameConfigUtils } from "../util/gameConfigHelpers";
+/* Constants */
+import { BOARD_WIDTH, BOARD_HEIGHT } from "../constants/gameConfig";
+import { IBoardCoords } from "../constants/types";
+
+export default (ctx: CanvasRenderingContext2D) => {
+  const _drawUtils = drawUtils(ctx);
+  const _gameConfigUtils = gameConfigUtils();
+  const boardCoords: IBoardCoords = _gameConfigUtils.createTileArray();
+
+  const clear = () => {
+    ctx.clearRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+  };
+
+  // draw tiles
+  const draw = () => {
+    clear();
+    console.log("Drawing...");
+    ctx.fillStyle = "red";
+    _drawUtils.drawTiles(boardCoords);
+  };
+
+  const init = () => {
+    console.log("Initializing game...");
+    return draw();
+    // return setInterval(draw, 500);
+  };
+
+  return init();
+};
